746cfc26c51dd771decbaf73d4071ce5ed7b203f
chiliwis <cristinasanchezg@gmail.com>
2022-09-24 10:27:38 -0500

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 console.log("Entro al main.js");
+
+const animalRandom = document.getElementById ('animalRandom');
+const URL = "http://ucamp.alumnos.dev4humans.com.mx/Main/endpoint_animales_zoo";
+
+const randomZoo = () => {
+    console.log(entro a la funcion randomZoo);
+    fetch (URL)
+    .then((data) => {
+        console.log(data);
+        animalRandom =data.url;
+    })
+    catch((error) => {
+        console.log("Ocurrio un error");
+    });
+}
